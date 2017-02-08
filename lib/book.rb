d76296accbcc1a52e09a55564996db468be04cf9
Wick007 <a.wick17@gmail.com>
2017-02-08 16:37:13 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,23 @@
+####make list of work flow/algorithm
+###Dash documentation app
+
+#1. have the class keep track of all of the genres of all the books we create
+#2.
+
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
   end
 
   def turn_page
